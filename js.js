27f741257fc8434ed50864022c0324148fb7aa94
danilogoncalves94 <danilogoncalvesdg21@gmail.com>
2023-03-17 16:12:13 -0300

    Arquivos adicionados

---
+++ js.js
@@ -0,0 +1,64 @@
+function clik1() {
+    setTimeout(() => {
+        $('body').css({
+            background: "url(/imagens/25-21-fundo-body.jpg) no-repeat center",
+            backgroundSize: 'cover'
+        })
+    }, 1000)
+}
+
+function clik2() {
+    setTimeout(() => {
+        $('body').css({
+            background: "url(/imagens/advogada-fundo-body.jpg) no-repeat center",
+            backgroundSize: 'cover'
+        })
+    }, 1000)
+}
+
+function clik3() {
+    setTimeout(() => {
+        $('body').css({
+            background: "url(/imagens/amor-rural-fundo-body.jpg) no-repeat center",
+            backgroundSize: 'cover'
+        })
+    }, 1000)
+}
+
+function clik4() {
+    setTimeout(() => {
+        $('body').css({
+            background: "url(/imagens/apostando-alto-fundo-body.jpg) no-repeat center",
+            backgroundSize: 'cover'
+        })
+    }, 1000)
+}
+
+function clik5() {
+    setTimeout(() => {
+        $('body').css({
+            background: "url(/imagens/pousando-no-amor-fundo-body.jpg) no-repeat center",
+            backgroundSize: 'cover'
+        })
+    }, 1000)
+}
+
+function clik6() {
+    setTimeout(() => {
+        $('body').css({
+            background: "url(/imagens/cha-cha-cha-fundo-body.jpg) no-repeat center",
+            backgroundSize: 'cover'
+        })
+    }, 1000)
+}
+
+/*function func1() {
+    /*$('div').css
+}*/
+
+/*function func2() {
+    $('div').animate({height: '720px'}, 2000)
+        /*visibility: 'visible',
+    $('img').animate({opacity: '1'}, 1000)
+    $('button').css('opacity', '0')
+}*/
